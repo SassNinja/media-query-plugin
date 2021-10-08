fbdf21c80c2131db31f0a36cbabe9cdd40c163a4
Yannick Lin <yannicklin.web@gmail.com>
2021-10-08 10:39:24 +1000

    Solved the issue of multi-entries. Using Entry name perferred.

--- src/loader.js
+++ src/loader.js
@@ -1,6 +1,6 @@
 /**
  * The loader component is supposed to extract the media CSS from the source chunks.
- * To do this it uses a custom PostCSS plugin. 
+ * To do this it uses a custom PostCSS plugin.
  * In the course of this the original media CSS gets removed.
  */
 
@@ -9,7 +9,7 @@ const postcss = require('postcss');
 const store = require('./store');
 const plugin = require('./postcss');
 
-module.exports = function(source) {
+module.exports = function (source) {
 
     // make loader async
     const cb = this.async();
@@ -24,20 +24,57 @@ module.exports = function(source) {
     // (don't use options.filename to avoid name conflicts)
     options.path = interpolateName(this, '[path][name].[ext]', {});
 
+
+    // get responding EntryPoint name
+    // for those using <Object> Entries
+    let webpackEntryPoint = '';
+    this._compilation.entries.forEach(entry => {
+        entry.dependencies.forEach(file => {
+
+            const FileEntry = file.request;
+            let FileEntry_Resolved = '';
+
+            if (FileEntry.startsWith("/") ||
+                (FileEntry.length > 1 && FileEntry[1] === ":")
+            ) {
+                FileEntry_Resolved = FileEntry;
+            } else {
+                FileEntry_Resolved = this.rootContext + FileEntry.replace(/^.\//, '/');
+            }
+
+            if ('' == webpackEntryPoint && FileEntry_Resolved === options.path) {
+                webpackEntryPoint = entry.options.name;
+            }
+        });
+    });
+    options.entrypoint = webpackEntryPoint;
+
+
     let isIncluded = false;
 
     // check if current file should be affected
-    if (options.include instanceof Array && options.include.indexOf(options.basename) !== -1) {
+    if (options.include === true) {
         isIncluded = true;
-    } else if (options.include instanceof RegExp && options.basename.match(options.include)) {
-        isIncluded = true;
-    } else if (options.include === true) {
-        isIncluded = true;
+    } else {
+        if (1 == Object.keys(this._compilation.entries).length) {
+            if (options.include instanceof Array && options.include.indexOf(options.basename) !== -1) {
+                isIncluded = true;
+            } else if (options.include instanceof RegExp && options.basename.match(options.include)) {
+                isIncluded = true;
+            }
+        }
+        else {
+            if (options.include instanceof Array && options.include.indexOf(options.entrypoint) !== -1) {
+                isIncluded = true;
+            } else if (options.include instanceof RegExp && options.entrypoint.match(options.include)) {
+                isIncluded = true;
+            }
+        }
     }
 
     // return (either modified or not) source
     if (isIncluded === true) {
-        postcss([ plugin(options) ])
+        postcss([plugin(options)])
             .process(source, { from: options.basename })
             .then(result => {
                 cb(null, result.toString())
